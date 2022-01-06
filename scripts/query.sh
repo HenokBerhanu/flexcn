a3e3e004e03da7bda8129cfca9dd8b6d1eb710db
Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>
2022-01-06 13:52:58 +0100

    feat(xapps): supports both flexcn in docker and on bare metal
    Signed-off-by: Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,1 +1,12 @@
-curl -X GET http://192.168.70.142:80/nxapp/v1/stats
+if [[ "$1" == "docker" ]]; then
+    FLEXCN_IP="192.168.70.142"
+elif [[ "$1" == "host" ]]; then
+    FLEXCN_IP="192.168.70.1"
+else
+    echo "This argument are not supported. Only [docker, host] are supported."
+    exit 1
+fi 
+
+echo "Assume we connectd to a single CN with IP: ${FLEXCN_IP}"
+
+curl -X GET http://${FLEXCN_IP}:80/nxapp/v1/stats
--- scripts/subscribe.sh
+++ scripts/subscribe.sh
@@ -1,9 +1,18 @@
-echo "Assume we connectd to a single CN:"
+if [[ "$1" == "docker" ]]; then
+    FLEXCN_IP="192.168.70.142"
+elif [[ "$1" == "host" ]]; then
+    FLEXCN_IP="192.168.70.1"
+else
+    echo "This argument are not supported. Only [docker, host] are supported."
+    exit 1
+fi 
+
+echo "Assume we connectd to a single CN with IP: ${FLEXCN_IP}"
 
 echo "Subscribe for AMF"
-curl -X GET http://192.168.70.142:80/nxapp/v1/trigger_subcribe/amf > /dev/null 2>&1
+curl -X GET http://${FLEXCN_IP}:80/nxapp/v1/trigger_subcribe/amf #> /dev/null 2>&1
 
 echo "Subscribe for SMF"
-curl -X GET http://192.168.70.142:80/nxapp/v1/trigger_subcribe/smf > /dev/null 2>&1
+curl -X GET http://${FLEXCN_IP}:80/nxapp/v1/trigger_subcribe/smf #> /dev/null 2>&1
 
 echo "Triggered FlexCN. Now connect RAN and UE to CN and try to query FlexCN."
